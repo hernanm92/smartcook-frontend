cb2af46bca4ed5b97f2ea87cbdc2b88abacad579
Hernan Maschwitz <hernanm992@gmail.com>
2016-05-30 03:00:47 -0300

    fix style

--- public/javascripts/controller/home.js
+++ public/javascripts/controller/home.js
@@ -1,5 +1,14 @@
 app.controller('HomeController',
     function ($scope, ingredientFactory, eventService) {
-        $scope.variable = 1;
+        $scope.$on('$viewContentLoaded', function(){
+            App.init();
+            App.initCounter();
+            Owl2Carouselv1.initOwl2Carouselv1();
+            Owl2Carouselv2.initOwl2Carouselv2();
+            Owl2Carouselv3.initOwl2Carouselv3();
+            Owl2Carouselv4.initOwl2Carouselv4();
+            Owl2Carouselv5.initOwl2Carouselv5();
+            ContactForm.initContactForm();
+        });
     }
 );
