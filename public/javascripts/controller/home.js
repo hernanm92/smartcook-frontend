2a289f33324e3e92d7ae58f49ced025a9793b60c
Hernan Maschwitz <hernanm992@gmail.com>
2016-06-05 17:56:15 -0300

    add ecommerce template

--- public/javascripts/controller/home.js
+++ public/javascripts/controller/home.js
@@ -2,13 +2,11 @@ app.controller('HomeController',
     function ($scope, ingredientFactory, eventService) {
         $scope.$on('$viewContentLoaded', function(){
             App.init();
-            App.initCounter();
-            Owl2Carouselv1.initOwl2Carouselv1();
-            Owl2Carouselv2.initOwl2Carouselv2();
-            Owl2Carouselv3.initOwl2Carouselv3();
-            Owl2Carouselv4.initOwl2Carouselv4();
-            Owl2Carouselv5.initOwl2Carouselv5();
-            ContactForm.initContactForm();
+            App.initScrollBar();
+            App.initParallaxBg();
+            OwlCarousel.initOwlCarousel();
+            RevolutionSlider.initRSfullWidth();
+            StyleSwitcher.initStyleSwitcher();
         });
     }
 );
