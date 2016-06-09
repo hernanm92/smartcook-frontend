69ad9f43c0bddd1182dd444de100871a67f08b18
Hernan Maschwitz <hernanm992@gmail.com>
2016-06-09 01:19:55 -0300

    listado de recetas

--- mock/app.js
+++ mock/app.js
@@ -10,13 +10,6 @@ var accessLogStream = fs.createWriteStream('mock.log', {flags: 'w'});
 app.use(morgan('common', {stream: accessLogStream}));
 
 app.use(bodyParser.json())
-var ingredients = [
-  {name: 'Pollo', category: 'Carne', image_url: 'img/chicken.png'},
-	{name: 'Papa', category: 'Tuberculo', image_url: 'img/potatoe.jpg'},
-  {name: 'Zapallo', category: 'Verdura', image_url: 'img/pumpkin.png'},
-  {name: 'Aceite de oliva', category: 'Aceite', image_url: 'img/olive_oil.png'},
-];
-
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin','*');
@@ -24,6 +17,20 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods','GET,POST,PUT,OPTIONS,DELETE');
   next();
 });
+
+var ingredients = [
+  {id: 1, name: 'Pollo', category: 'Carne', image_url: 'img/chicken.png'},
+	{id: 2, name: 'Papa', category: 'Tuberculo', image_url: 'img/potatoe.jpg'},
+  {id: 3, name: 'Zapallo', category: 'Verdura', image_url: 'img/pumpkin.png'},
+  {id: 4, name: 'Aceite de oliva', category: 'Aceite', image_url: 'img/olive_oil.png'}
+];
+
+var recipes = [
+  {id: 1, name: 'Pollo a la griega', image_url: 'img/chicken-food.jpg', stars: 4, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin erat nec ornarevolu tpat. Etiam ut felis nec nisl eleifend lobortis. Aenean nibh est, hendrerit non conva.'},
+  {id: 2, name: 'Pollo a la griega', image_url: 'img/chicken-food.jpg', stars: 4, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin erat nec ornarevolu tpat. Etiam ut felis nec nisl eleifend lobortis. Aenean nibh est, hendrerit non conva.'},
+  {id: 3, name: 'Pollo a la griega', image_url: 'img/chicken-food.jpg', stars: 4, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin erat nec ornarevolu tpat. Etiam ut felis nec nisl eleifend lobortis. Aenean nibh est, hendrerit non conva.'},
+  {id: 4, name: 'Pollo a la griega', image_url: 'img/chicken-food.jpg', stars: 4, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin erat nec ornarevolu tpat. Etiam ut felis nec nisl eleifend lobortis. Aenean nibh est, hendrerit non conva.'}
+];
 
 //ingredients
 app.get('/ingredients', function(req, res) {
