a517cc6a7e61e65606b97dae2cc4897212a3b134
Hernan Maschwitz <hernanm992@gmail.com>
2016-06-10 02:11:50 -0300

    twitter carrousel

--- public/javascripts/controller/validate.js
+++ public/javascripts/controller/validate.js
@@ -6,6 +6,7 @@ app.controller('ValidateController',
             MouseWheel.initMouseWheel();
             StyleSwitcher.initStyleSwitcher();
             RevolutionSlider.initRSfullWidth();
+            App.initParallaxBg();
         });
 
         $scope.getRecipes = function(){
