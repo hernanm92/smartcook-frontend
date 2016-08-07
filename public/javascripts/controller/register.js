db9d9fae62a5806b6619e55307c89a9a62e3f6a8
agustinmodugno <agustinmodugno@hotmail.com>
2016-08-07 12:20:01 -0300

    Agrego datepicker a Registrarse, BUG: No lo abre cuando apreto el boton

--- public/javascripts/controller/register.js
+++ public/javascripts/controller/register.js
@@ -15,11 +15,42 @@ app.controller('RegisterController',
             email : '',
             password : ''
         };
+
+
+//---------------------------------DATE PICKER SECTION---------------------------------
+        $scope.today = function() {
+            $scope.dt = new Date();
+          };
+        $scope.today();
+
+        $scope.clear = function() {
+            $scope.dt = null;
+          };
+
+        $scope.format = 'dd-MMMM-yyyy';
+
+        $scope.popup1 = {
+            opened: false
+        };
+
+        $scope.open1 = function() {
+            $scope.popup1.opened = true;
+        };
+
         $scope.myDate = new Date();
         $scope.registerMaxDate = new Date(
-              $scope.myDate.getFullYear(),
-              $scope.myDate.getMonth(),
-              $scope.myDate.getDate()-1);
+                      $scope.myDate.getFullYear(),
+                      $scope.myDate.getMonth(),
+                      $scope.myDate.getDate()-1);
+        $scope.dateOptions = {
+            maxDate: $scope.registerMaxDate,
+            showWeeks:true,
+            startingDay: 1
+        };
+
+
+
+
 
         $scope.test = function() { console.log($scope.user.gender) };
     }
