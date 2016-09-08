e84665296a99cecad4b44f2f3f8ac9c62a2b38e8
matiasleon <matias.leon.peralta@gmail.com>
2016-09-08 00:06:39 -0300

    Add: crete recipe + view integraation

--- public/javascripts/factory/ingredientPerRecipes.js
+++ public/javascripts/factory/ingredientPerRecipes.js
@@ -1,7 +1,7 @@
 var module = angular.module('ingredientPerRecipeModule', ['ngResource']);
 
 module.factory('ingredientPerRecipeFactory', ['$resource', 'config', function ($resource, config) {
-    return $resource(config.domain + '/ingredients_per_recipes/:id', {id: "@id"}, {
+    return $resource(config.domain + '/ingredients_per_recipes/recipe/:id/ingredient, {id: "@id",}, {
     	update: {
     		method: 'PUT'
     	}
