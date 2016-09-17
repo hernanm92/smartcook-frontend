84518ff265a89b2294a660020c8b6b4a80f38704
agustinmodugno <agustinmodugno@hotmail.com>
2016-09-16 22:15:33 -0300

    Agrego logica de cargar comensales ya agregados. Falta darle funcionalidad al boton estrella para agregar/remover

--- public/javascripts/controller/addCommensal.js
+++ public/javascripts/controller/addCommensal.js
@@ -1,22 +1,42 @@
 app.controller('AddCommensalController',
-    function ($scope, userFactory) {
+    function ($scope, userFactory, frequentCommensalFactory, UserSession) {
         $scope.searchedUser = "";
         $scope.resultUsers = [];
         $scope.updateSearchedUsers = updateSearchedUsers;
         $scope.hoverAddCommensalButton = hoverAddCommensalButton;
+        $scope.addCommensal = addCommensal;
+        $scope.removeCommensal = removeCommensal;
         $scope.addCommensalButtonHover = false;
+
+        $scope.users = userFactory.query();
+        UserSession.profileInfo().$promise.then(function(val){
+            $scope.currentUser = val.userName;
+            frequentCommensalFactory.query({username: $scope.currentUser},function(results){
+                $scope.frequentUsers = results;
+                $scope.frequentCommensals = $($scope.users).filter(function() {
+                    return $scope.frequentUsers.indexOf(this.userName.toLowerCase()) >= 0;
+                });
+            });
+        });
+
+        function updateSearchedUsers(){
+            $scope.resultUsers = $($scope.users).filter(function() {
+                return (this.userName.toLowerCase().indexOf($scope.searchedUser.toLowerCase()) != -1
+                    && $scope.frequentUsers.indexOf(this.userName.toLowerCase()) == -1);
+            });
+        };
 
         function hoverAddCommensalButton($event){
             $($event.currentTarget).toggleClass('fa-star-o').toggleClass('fa-star');
         }
 
-        function updateSearchedUsers(){
-            var users = userFactory.query(function(){
-                $scope.resultUsers = $(users).filter(function() {
-                return this.userName.toLowerCase().indexOf($scope.searchedUser.toLowerCase()) != -1;
-                });
-            });
-        };
+        function addCommensal($event){
+
+        }
+
+        function removeCommensal($event){
+
+        }
 
     }
 );
