bade1eb23c5fbc4b868314d503bc919787548720
matiasleon <matias.leon.peralta@gmail.com>
2016-09-17 11:35:33 -0300

    mod: countdownd + fix validate

--- public/javascripts/controller/validate.js
+++ public/javascripts/controller/validate.js
@@ -2,59 +2,57 @@ app.controller('ValidateController',
     function ($scope, recipeFactory, validateFactory, eventService, $timeout) {
         $scope.validateRecipeIndex = 0;
         $scope.validateCurrentRecipe = {};
-        $("body").keypress(function(e){
-           if(e.which == '108') $scope.nextRecipe(1);
-           if(e.which == '100') $scope.nextRecipe(0);
+        $scope.recipes = [];
+        $("body").keypress(function (e) {
+            if (e.which == '108') $scope.nextRecipe(1);
+            if (e.which == '100') $scope.nextRecipe(0);
         });
 
-        function getRecipes(){
-            recipeFactory.query({},function(recipes){
-                $scope.recipes=recipes;
-                $scope.validateCurrentRecipe = recipes[0];
+        init();
+        function init() {
+            recipeFactory.query({}, function (recipes) {
+                $scope.recipes = recipes;
+                $scope.validateCurrentRecipe = $scope.recipes[0];
             });
-        };
+        }
 
-        function setNextRecipe(){
-            if($scope.validateRecipeIndex < ($scope.recipes.length - 1)){
+        function setNextRecipe() {
+            if ($scope.validateRecipeIndex < ($scope.recipes.length - 1)) {
                 $scope.validateRecipeIndex++;
                 $scope.validateCurrentRecipe = $scope.recipes[$scope.validateRecipeIndex];
-            }else{
-                $('.validateRecipe-Recipe').css('visibility','hidden');
-                $.notify("No hay mas recetas para validarr",{globalPosition:'right bottom',className:'info'});
+            } else {
+                $('.validateRecipe-Recipe').css('visibility', 'hidden');
+                $.notify("No hay mas recetas para validarr", { globalPosition: 'right bottom', className: 'info' });
             }
         }
 
-        getRecipes();
-
-        $scope.runBootstro = function(){
-            bootstro.start('.bootstro',{
+        $scope.runBootstro = function () {
+            bootstro.start('.bootstro', {
                 nextButtonText: 'Siguiente »',
                 prevButtonText: '« Anterior',
                 finishButtonText: 'Finalizar DEMO'
             });
         };
 
-        $scope.nextRecipe = function(validation){
-            if($('.validateRecipe-Recipe').css('visibility') != "hidden"){
-                $('.validateRecipe-Recipe').animate({opacity: 0}, 500,function(){
+        $scope.nextRecipe = function (validation) {
+            if ($('.validateRecipe-Recipe').css('visibility') != "hidden") {
+                $('.validateRecipe-Recipe').animate({ opacity: 0 }, 500, function () {
                     var validatedRecipe = {
-                        "userId":1, //TODO: De donde se saca?
+                        "userId": 1, //TODO: De donde se saca?
                         "recipeId": $scope.validateCurrentRecipe.id,
                         "validation": validation
                     };
-                    $scope.validate = new validateFactory();
-                    $scope.validate.data = validatedRecipe;
-                    validateFactory.save($scope.validate,function (response) {
-                        $.notify("Receta Validada",{globalPosition:'right bottom',className:'success'});
+                    validateFactory.save(validatedRecipe, function (response) {
+                        $.notify("Receta Validada", { globalPosition: 'right bottom', className: 'success' });
                     });
-                    $timeout(function(){
+                    $timeout(function () {
                         setNextRecipe();
-                        $('.validateRecipe-Recipe').animate({opacity: 1}, 500);
+                        $('.validateRecipe-Recipe').animate({ opacity: 1 }, 500);
                     }
-                    ,1000);
+                        , 1000);
                 });
             }
-            else $.notify("No hay mas recetas para validar",{globalPosition:'right bottom',className:'info'});
+            else $.notify("No hay mas recetas para validar", { globalPosition: 'right bottom', className: 'info' });
         };
 
     }
