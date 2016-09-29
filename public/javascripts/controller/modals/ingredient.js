c33d996f4efbb176f88729cbaaf4fe06fa5ae236
matiasleon <matias.leon.peralta@gmail.com>
2016-09-29 16:25:02 -0300

    Fix: detalle ing categorias + login diseño

--- public/javascripts/controller/modals/ingredient.js
+++ public/javascripts/controller/modals/ingredient.js
@@ -1,9 +1,17 @@
 app.controller('IngredientModalController',
-    function ($scope, item,$modalInstance) {
+    function ($scope, item, $modalInstance, categoriesFactory) {
         $scope.ingredient = item;
         $scope.ok = function () {
             $modalInstance.close();
         };
+        //aca traer las descripcion de la categoria
+        init();
+
+        function init() {
+            categoriesFactory.get({id:$scope.ingredient.food_category_id}, function (category) {
+                $scope.ingredient.food_category = category.name;  
+            })
+        }
 
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
--- public/javascripts/factory/category.js
+++ public/javascripts/factory/category.js
@@ -1,5 +1,5 @@
 var module = angular.module('categoryModule', ['ngResource']);
 
 module.factory('categoriesFactory', ['$resource', 'config', function ($resource, config) {
-    return $resource(config.domain + '/food_categories');
+    return $resource(config.domain + '/food_categories/:id', { id: '@id' });
 }]);
