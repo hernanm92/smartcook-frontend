572da98c64d27c06eaf2c23c0eb45d426f208d25
matiasleon <matias.leon.peralta@gmail.com>
2016-10-31 19:21:28 -0300

    add 10 users data

--- Scripts/production/Tables/migrate sequence.sql
+++ Scripts/production/Tables/migrate sequence.sql
@@ -2,6 +2,6 @@ ALTER SEQUENCE food_categories_id_seq RESTART WITH 17;
 ALTER SEQUENCE ingredients_id_seq RESTART WITH 272;
 ALTER SEQUENCE recipes_id_seq RESTART WITH 100;
 ALTER SEQUENCE ingredients_per_recipes_id_seq RESTART WITH 710;
-ALTER SEQUENCE users_id_seq RESTART WITH 4;
+ALTER SEQUENCE users_id_seq RESTART WITH 11;
 
 
