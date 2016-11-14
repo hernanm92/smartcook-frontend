d6fbff4bf3b2ca922e86d0caa3447fc24afa6aad
Hernan Maschwitz <hernanm992@gmail.com>
2016-11-13 23:50:06 -0300

    general fixes

--- public/javascripts/service/restrictions.js
+++ public/javascripts/service/restrictions.js
@@ -6,12 +6,12 @@ function restrictionsService() {
     var self = this;
     self.getRestrictions = getRestrictions;
     self.mapRestrictions = mapRestrictions;
-    self.restrictions = [{ name: 'Celiaco' }, { name: 'Diabetico' }, { name: 'Vegano' }, { name: 'Vegetariano' }];
+    self.restrictions = [{ name: 'Celíaco' }, { name: 'Diabético' }, { name: 'Vegano' }, { name: 'Vegetariano' }];
 
     function mapRestrictions(recipe) {
         var restrictions = [];
-        recipe.diabetic ? restrictions.push('Diabetico') : null;
-        recipe.celiac ? restrictions.push('Celiaco') : null;
+        recipe.diabetic ? restrictions.push('Diabético') : null;
+        recipe.celiac ? restrictions.push('Celíaco') : null;
         recipe.vegan ? restrictions.push('Vegano') : null;
         recipe.vegetarian ? restrictions.push('Vegetariano') : null;
         return restrictions;
